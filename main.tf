194a37f845e027b335b4c3ee1106c600d6c51f0e
sumanthtony <sumanthbattagiri99@gmail.com>
2026-04-15 15:45:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ iname = "IAC-SERVER"
 itype = "t3.micro"
 icount = 1
 ami_id = "ami-0aa31b568c1e8d622"
+vpc_security_group_ids = var.vpc_security_group_ids  #Created a variable in Instance-module section and called here
 }
 
 module "s3-bucket-module" {
